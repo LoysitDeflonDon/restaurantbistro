5d6b8c3d0b7905acf459f3c62943a5419f735aef
LoysitDeflonDon <foxvladfokin@yandex.ru>
2026-06-15 01:23:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // ========== НАСТРОЙКИ JSONBIN ==========
 // ЗАМЕНИ ЭТИ ТРИ СТРОКИ НА СВОИ ДАННЫЕ!
 const MASTER_KEY = '$2a$10$LX5oylJg7sUo03LV/.UGeeeJB8Qbq3FHt1N8uL9zvuzCCW4wDxNX.';           // API ключ с jsonbin.io
-const USERS_BIN_ID = '6a2f2c6eda38895dfec07f8a';        // ID бина с пользователями
-const BOOKINGS_BIN_ID = '6a2f2c7bf5f4af5e29f20087';     // ID бина с бронями
+const USERS_BIN_ID = '6a2f379cf5f4af5e29f221a6';        // ID бина с пользователями
+const BOOKINGS_BIN_ID = '6a2f37b3da38895dfec0a14e';     // ID бина с бронями
 
 let currentUser = null;
 let menuData = [];
